2cdd202b6f70d454b50e2d75e9c20adef50217c9
noobie007-sigma <sukjais7120@gmail.com>
2026-04-10 10:09:45 +0530

    microservices assignment done

---
+++ microservieceassignment/CartService/src/main/java/com/example/demo/CartServiceApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartServiceApplication.class, args);
+    }
+
+    // Register RestTemplate as a Bean
+    // CartService uses this to call other services
+    @Bean
+    @LoadBalanced 
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
